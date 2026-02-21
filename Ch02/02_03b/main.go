ffb50e6f4f36033df16e60af04a26c6a14419d23
Ananiya Belew <ananiya@arifpay.org>
2026-02-21 12:54:46 +0300

    scanning User input

--- Ch02/02_03b/main.go
+++ Ch02/02_03b/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
+// Reading user input with type Conversion
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Please Enter Your name: ")
+	str, _ := reader.ReadString('\n')
+	fmt.Printf("Hello Mr/Ms %s\n", str)
+	fmt.Print("Please Enter Your Age: ")
+	age, _ := reader.ReadString('\n')
+	parsedAge, err := strconv.ParseInt(strings.TrimSpace(age), 0, 64)
+	if err != nil {
+		fmt.Print("Error Parsing Your Age", err)
+	} else {
+		fmt.Printf("Great Your Age is: %d\n", parsedAge)
+	}
 }
